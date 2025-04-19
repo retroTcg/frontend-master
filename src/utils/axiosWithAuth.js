6ce52b5dae1fdc5342fae05244df358f0dab9ceb
Charles <charley43613@gmail.com>
2025-04-19 03:12:48 -0400

    trial

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const axiosWithAuth = () => {
+	console.log("LOGGING TOKEN HERE: " + localStorage.getItem('token') + " as string : " + JSON.stringify(localStorage.getItem('token')))
 	const token = localStorage.getItem('token');
 
 	return axios.create({
